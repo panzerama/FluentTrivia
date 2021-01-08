0bd64c6c73fb3635d6130cbbfb16be8bf511cd8f
Jason Panzer <jason.drew.panzer@gmail.com>
2021-01-07 20:14:11 -0800

    Adds questions and answer display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import { QuestionPreview } from './components/QuestionPreview'
-import { QuestionDisplay } from './components/QuestionDisplay'
-import { Question } from './components/Question'
-import { DemoQuestions } from './mockData/DemoQuestions'
+import React from "react";
+import { Header } from "./components/Header";
+import { QuestionPreview } from "./components/QuestionPreview";
+import { QuestionDisplay } from "./components/QuestionDisplay";
+import { Question } from "./components/Question";
+import { DemoQuestions } from "./mockData/DemoQuestions";
 
 function App() {
-  const [questions, setQuestions] = React.useState(DemoQuestions)
-  const [currentQuestion, setCurrentQuestion] = React.useState <Question>()
+  const [questions, setQuestions] = React.useState(DemoQuestions);
+  const [currentQuestion, setCurrentQuestion] = React.useState<Question>();
+
+  const questionDisplay = currentQuestion ? (
+    <QuestionDisplay question={currentQuestion} />
+  ) : (
+    ""
+  );
 
   return (
-    <div>
-      <button>Practice Mode</button>
-      <QuestionDisplay question={currentQuestion}/>
-      {questions.map((question) => { 
-        return <QuestionPreview question={question as Question} questionSelectionHandler={setCurrentQuestion}/>
-      })}
+    <div className="container">
+      <Header />
+      <div>
+        <button>Practice Mode</button>
+        {questionDisplay}
+        {questions.map((question) => {
+          return (
+            <QuestionPreview
+              question={question as Question}
+              questionSelectionHandler={setCurrentQuestion}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- client/src/components/QuestionDisplay.tsx
+++ client/src/components/QuestionDisplay.tsx
@@ -3,7 +3,7 @@ import { Question } from './Question'
 import CSS from 'csstype'
 
 export type QuestionDisplayProps = {
-  question?: Question
+  question: Question
   questionHandler?: () => boolean
 }
 
@@ -11,10 +11,61 @@ const styles: CSS.Properties = {
   margin: '20px'
 }
 
+type Answer = {
+  description: string
+  correct: boolean
+}
+
 export const QuestionDisplay = ({ question, questionHandler }: QuestionDisplayProps) => {
+  const randomizeElements = (arr: [Answer]) => {
+
+    let randomIndex = Math.floor(Math.random() * Math.floor(arr.length))
+    let randomized: [Answer] = [arr[randomIndex]]
+    arr.splice(randomIndex, 1)
+
+    console.log(arr)
+
+    while (arr.length > 0) {
+      randomIndex = Math.floor(Math.random() * Math.floor(arr.length))
+      randomized.push(arr[randomIndex])
+      arr.splice(randomIndex, 1)
+    }
+
+    return randomized
+  }
+
+  const compileAnswers = (question: Question) => {
+    let answers: [Answer] = [
+      {
+        description: question.correct_answer,
+        correct: true
+      }
+    ]
+
+    question.incorrect_answers.map((answer) => {
+      answers.push(
+        {
+          description: answer,
+          correct: false
+        }
+      )
+    })
+
+    return answers
+  }
+
+  const randomizedAnswers = randomizeElements(compileAnswers(question))
+
   return (
     <div style={styles}>
-      <span>{question?.question}</span>
+      <span>{question.question}</span>
+      <ul>
+        {/* workitem: randomize the display of correct v. incorrect */}
+        <li>{question.correct_answer}</li>
+        {question.incorrect_answers.map((answer) => {
+          return <li>{answer}</li>
+        })}
+      </ul>
     </div>
   )
 }
